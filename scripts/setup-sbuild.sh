da265283e65ab0368ebfd706252f833af0780deb
Kazuhiro Hayashi <kazuhiro3.hayashi@toshiba.co.jp>
2018-10-12 12:40:00 +0000

    setup-sbuild.sh: helper script to setup chroots for sbuild
    Signed-off-by: Kazuhiro Hayashi <kazuhiro3.hayashi@toshiba.co.jp>

---
+++ scripts/setup-sbuild.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# assumes to be called in the build directory
+LOCALCONF=$(pwd)/conf/local.conf
+CHROOT_DIR=$(pwd)/chroot
+
+die() {
+	echo "ERROR: ${@}"
+	exit 1
+}
+
+if [ "$(whoami)" != "root" ]; then
+	die "Please run this script as root"
+fi
+
+if [ ! -f ${LOCALCONF} ]; then
+	die "${LOCALCONF} not found"
+fi
+
+# pull variables defined in LOCALCONF
+for var in DEBIAN_CODENAME DEBIAN_ARCH DEBIAN_REPOS; do
+	val=$(grep "${var}\s*=" ${LOCALCONF} | tail -1 | \
+	      sed "s@${var}\s*=\s*\(.*\)@\1@")
+	if [ -z "${val}" ]; then
+		die "${var} is not defined in ${LOCALCONF}"
+	fi
+	eval ${var}=${val}
+done
+
+sbuild-createchroot \
+	${DEBIAN_CODENAME} \
+	${CHROOT_DIR}/${DEBIAN_CODENAME}-${DEBIAN_ARCH} \
+	${DEBIAN_REPOS} \
+	|| die "sbuild-createchroot failed"
