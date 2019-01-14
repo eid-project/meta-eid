40964139f20e9e5c485ac79ee27af7f7bb0d4920
Trung Do <trung.dothanh@toshiba-tsdv.com>
2019-01-14 10:26:13 +0700

    test.sh: Support build test with proxy
    Add environment variables for proxy.
    
    Some examples of recipe are fetching source with git,
    proxy should be preconfigured for git, too.
    
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

--- test.sh
+++ test.sh
@@ -9,6 +9,9 @@ META_EID_DIR=$(dirname $(readlink -f "$0"))
 E="docker exec -it -u1000 $CNAME /bin/bash -c "
 
 docker run \
+	--env http_proxy="$http_proxy" \
+	--env https_proxy="$https_proxy" \
+	--env no_proxy="$no_proxy" \
 	--workdir /home/eid \
 	--cap-add SYS_ADMIN \
 	-v $META_EID_DIR:/home/eid/poky/meta-eid:ro \
@@ -18,6 +21,11 @@ docker run \
 	-d \
 	--name $CNAME \
 	$IMAGENAME
+
+# git proxy
+test -n "$http_proxy" && $E "git config --global http.proxy $http_proxy"
+test -n "$https_proxy" && $E "git config --global https.proxy $https_proxy"
+test -n "$no_proxy" && $E "git config --global core.noproxy $no_proxy"
 
 # rebuild chroot
 $E "sudo rm -rf /etc/schroot/chroot.d/buster-amd64-* /home/eid/build/buster-amd64-*"
